ae4c70c3f7b9e5d1e26b4f9da7efe0fdea713d41
HenryqueProgramming2025 <henryquecarlos2020@gmail.com>
2025-09-23 20:56:36 +0000

    Título, vetor e uso do scanf

--- war.c
+++ war.c
@@ -8,3 +8,24 @@ typedef struct{
     char cor [10];
     int tropas;
 } Territorio;
+
+int main(){
+
+    printf("=== Seja muito bem-vindo(a) ao jogo de estratégia War! ===");
+    printf("Abaixo, entre com o nome, a cor e a quantidade de tropas:\n\n");
+
+    // Declarando o Vetor
+
+    Territorio territorios[5];
+
+    for (int i = 0; i < 5; i++) {
+        printf("Território %d:\n", i + 1);
+        printf("Nome: ");
+        scanf("%s", territorios[i].nome);
+        printf("Cor: ");
+        scanf("%s", territorios[i].cor);
+        printf("Quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+    }
+
+}
