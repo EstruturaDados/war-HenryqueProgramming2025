0ebf14a1182fe5f4a1b8bf92e561b0497bd423fa
HenryqueProgramming2025 <henryquecarlos2020@gmail.com>
2025-09-23 21:42:20 +0000

    Código Aventureiro Finalizado

--- war.c
+++ war.c
@@ -9,9 +9,9 @@ typedef struct{
     int tropas;
 } Territorio;
 
-int main(){
+int main() {
 
-    printf("=== Seja muito bem-vindo(a) ao jogo de estratégia War! ===");
+    printf("=== Seja muito bem-vindo(a) ao jogo de estratégia War! ===\n\n");
     printf("Abaixo, entre com o nome, a cor e a quantidade de tropas:\n\n");
 
     // Declarando o Vetor
@@ -19,28 +19,31 @@ int main(){
     Territorio territorios[5];
 
     for (int i = 0; i < 5; i++) {
-        printf("Território %d:\n", i + 1);
+        printf("Território %d:\n\n", i + 1);
         printf("Nome: ");
-        scanf("%s", territorios[i].nome);
+        scanf("%49s", territorios[i].nome);
         printf("Cor: ");
-        scanf("%s", territorios[i].cor);
+        scanf("%9s", territorios[i].cor);
         printf("Quantidade de tropas: ");
         scanf("%d", &territorios[i].tropas);
-    }
 
-        while (getchar () != 'n'); // Limpeza de buffer
+       while (getchar() != '\n');
+    }
 
     // Exibindo os detalhes dos territórios
 
-printf("=== Detalhes dos territórios cadastrados ===\n\n");
+printf("\n=== Detalhes dos territórios cadastrados ===\n\n");
+
+fflush(stdout);
 
 for (int i = 0; i < 5; i++) {
     printf("Território %d:\n\n", i + 1);
     printf("Nome: %s\n", territorios[i].nome);
     printf("Cor: %s\n", territorios[i].cor);
     printf("Quantidade de tropas: %d\n\n", territorios[i].tropas);
+
+    fflush(stdout);
 }
 
 return 0;
-
 }
